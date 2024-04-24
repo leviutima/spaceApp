851f8d27b51fcaf0d59630617fcf0aaace3b78e4
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-24 12:44:34 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+const FundoGradiente = styled.div` 
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <FundoGradiente>
+
+      </FundoGradiente>
+    </>
+  )
+}
+
+export default App
