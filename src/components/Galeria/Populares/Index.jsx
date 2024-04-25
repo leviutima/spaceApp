4cc4ad4f941603b63d5a8a272d064432d5aed8a4
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-25 00:24:36 -0300

    incluindo fotos na section populares, consumindo um json

--- src/components/Galeria/Populares/Index.jsx
+++ src/components/Galeria/Populares/Index.jsx
@@ -1,9 +1,24 @@
+import styled, { keyframes } from "styled-components"
 import Titulo from "../../Titulo/Index"
+import Imagens from "../Imagens/Index"
+import fotos from './fotos-populares.json'
+
+const ContainerPopular = styled.div `
+
+`
 
 
 const Populares = () => {
     return <div style={{ minWidth: 212 }}>
         <Titulo $alinhamento="center">Populares</Titulo>
+        <ContainerPopular>
+            {fotos.map(foto => <Imagens
+            key={foto.id}
+            src={foto.path}
+            alt={foto.alt}
+            fotos={foto}/>)
+            }
+        </ContainerPopular>
     </div>
 }
 
--- src/components/ModalZoom/Index.jsx
+++ src/components/ModalZoom/Index.jsx
@@ -1,11 +1,35 @@
+import styled from "styled-components"
+import Imagens from "../Galeria/Imagens/Index"
+
+const OverLay = styled.div `
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
+
 const ModalZoom = ({ fotos }) => {
     return(
-        <dialog open = {!!fotos}>
+        <>
+            {fotos && <>
+        <OverLay/>
+        <DialogEstilizado open = {!!fotos}>
+            <Imagens fotos={fotos} expandida={true}/>
             <p>Exemplo do Mozilla</p>
                 <form method="dialog">
                     <button type="submit">ok</button>
                 </form>
-        </dialog>
+        </DialogEstilizado>
+            </>}
+        </>
     )
 }
 
